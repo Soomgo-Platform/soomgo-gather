39ebc826ef6a4fd9410e44f4bacad2cc653144a3
rosa-soomgo <rosa@soomgo.com>
2021-11-18 17:48:20 +0900

    logging??

--- src/soomgogather/naver/searchad.py
+++ src/soomgogather/naver/searchad.py
@@ -1,5 +1,6 @@
+import logging
 import requests
-from soomgogather.naver.request import RequestSearchAd
+from soomgogather.naver.request_searchad import RequestSearchAd
 
 class Bizmoney(RequestSearchAd):
 
@@ -12,12 +13,21 @@ class Bizmoney(RequestSearchAd):
 
     def get_report_period(self,start_dt, end_dt):
         uri = '/billing/bizmoney/histories/period'
-        r = requests.get(
-            self.base_url + uri,
-            params={
-                'searchStartDt': start_dt,
-                'searchEndDt': end_dt,
-            },
-            headers=self.get_header('GET', uri),
-        )
+        logging.info(f"Start requsting a bizmoney history during period {start_dt} and {end_dt}")
+
+        try:
+            r = requests.get(
+                self.base_url + uri,
+                params={
+                    'searchStartDt': start_dt,
+                    'searchEndDt': end_dt,
+                },
+                headers=self.get_header('GET', uri),
+            )
+            r.raise_for_status()
+
+        except requests.exceptions.HTTPError as e:
+            logging.exception(f"HTTP requests is failed {e}")
+            raise RuntimeError("HTTP requests is failed.")  
         return r
+
