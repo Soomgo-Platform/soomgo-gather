3f5d9c14afdbd4e56411f1a34423dcd9c1d961a7
rosa-soomgo <rosa@soomgo.com>
2021-11-22 14:40:13 +0900

    add validate params

--- src/soomgogather/naver/bizmoney.py
+++ src/soomgogather/naver/bizmoney.py
@@ -1,23 +1,33 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, ValidationError
 from ._searchad import BaseSearchAD
 
 
 class Bizmoney(BaseSearchAD):
-    def status(self):
-        return self.call('GET', '/billing/bizmoney')
 
     class _BizmoneySchema(Schema):
         search_start_dt = fields.Str(data_key='searchStartDt', required=True)
         search_end_dt = fields.Str(data_key='searchEndDt', required=True)
+        stat_dt = fields.Str(data_key='statDt')
+    
+    def set_params(self,params):
+        try:
+            request_params = self._BizmoneySchema().dump(params)
+            self._BizmoneySchema().load(request_params)
+            return request_params
+        except ValidationError as err:
+            raise ValueError(f"incorrect parameters: {err}")
+
+    def status(self):
+        return self.call('GET', '/billing/bizmoney')
 
     def cost(self, params={}):
-        return self.call('GET', '/billing/bizmoney/cost', params=self._BizmoneySchema().dump(params))
+        return self.call('GET', '/billing/bizmoney/cost', params=self.set_params(params))
 
     def charge(self, params={}):
-        return self.call('GET', '/billing/bizmoney/histories/charge', params=self._BizmoneySchema().dump(params))
+        return self.call('GET', '/billing/bizmoney/histories/charge', params=self.set_params(params))
 
     def exhaust(self, params={}):
-        return self.call('GET', '/billing/bizmoney/histories/exhaust', params=self._BizmoneySchema().dump(params))
+        return self.call('GET', '/billing/bizmoney/histories/exhaust', params=self.set_params(params))
 
     def period(self, params={}):
-        return self.call('GET', '/billing/bizmoney/histories/period', params=self._BizmoneySchema().dump(params))
+        return self.call('GET', '/billing/bizmoney/histories/period', params=self.set_params(params))
--- tests/test_naver.py
+++ tests/test_naver.py
@@ -2,9 +2,9 @@ from soomgogather.naver import Bizmoney
 import requests_mock
 
 bizmoney = Bizmoney(api_key='_', secret_key='_', customer_id='_')
+# bizmoney = Bizmoney(api_key='0100000000b47f8309b6e4c457716929c5b29d283ea13538a12391dbb9780b570970af5845', secret_key='AQAAAAC0f4MJtuTEV3FpKcWynSg+CU2VXAZjgpI0rmnttAJCrA==', customer_id='902486')
 
 params = {
-    'start_dt': '20211118',
     'search_start_dt': '20211118',
     'search_end_dt': '20211118',
 }
@@ -19,8 +19,22 @@ def test_bizmoney_status():
 def test_bizmoney_cost():
     with requests_mock.Mocker() as _mock:
         _mock.get(f'{bizmoney.domain}/billing/bizmoney/cost', status_code=200, json='_')
+
         assert bizmoney.cost(params=params).status_code == 200
 
+
+def test_bizmoney_cost_fail():
+    params = {
+        'search_start_dt': '20211118',
+    }
+
+    with requests_mock.Mocker() as _mock:
+        _mock.get(f'{bizmoney.domain}/billing/bizmoney/cost', status_code=200, json='_')
+        
+        try: 
+            bizmoney.cost(params=params)
+        except Exception as err:
+            assert type(err) == ValueError
 
 def test_bizmoney_charge():
     with requests_mock.Mocker() as _mock:
