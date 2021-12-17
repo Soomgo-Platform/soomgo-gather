df852e5283cf5e8792c4611d0a539e0f07f0eee5
paul <paul@soomgo.com>
2021-12-17 16:15:33 +0900

    Update setup.py
    fixed install_requires=[] to nstall_requires=install_requires

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
     python_requires=">=3.6",
-    install_requires=[],
+    install_requires=install_requires,
     extras_require={"test": tests_requires},
     zip_safe=False,
     # test_suite=''
