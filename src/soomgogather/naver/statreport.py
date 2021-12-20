90464f5842bd5e0053af6cdebf7455083e18eaec
hyooj10 <hyooj10@gmail.com>
2021-12-20 15:47:51 +0900

    임시 커밋

--- src/soomgogather/naver/statreport.py
+++ src/soomgogather/naver/statreport.py
@@ -6,20 +6,18 @@ from ._searchad import BaseSearchAD
 class StatReport(BaseSearchAD):
     """Naver SearchAd API StatReport
 
-    Naver SearchAd API에서 발급받은 api_key, secret_key, customer_id를 사용하여 MasterReport 클래스 객체를 생성한다.
-    생성한 MasterReport 객체로 마스터 기능을 이용하여 종류별 광고 정보 일괄 다운로드(마스터 데이터)하고 각 광고 정보를 확인할 수 있다.
+    Naver SearchAd API에서 발급받은 api_key, secret_key, customer_id를 사용하여 StatReport 클래스 객체를 생성한다.
+    생성한 StatReport 객체로 대용량 보고서 기능을 이용하여 특정일에 발생한 대용량 보고서 다운로드(Stat 데이터)하고 광고 효과 보고서를 확인할 수 있다.
 
-    광고 정보 일괄 다운로드는 특정 시점을 기준으로 광고 계정에 등록된 모든 광고 정보를 다운로드하는 기능으로 상시로 변경되는 정보는 제공되지 않으며 사용자가 등록한 광고 정보만 제공된다.
-    다운로드된 광고 정보는 각각 종류별 정보만 담고있다.
+    대용량 보고서 다운로드는 계정 단위로 특정일에 발생한 광고 효과 보고서를 다운로드하는 기능으로 일 단위로만 신청 가능하며 기간별 조회 기능은 제공되지 않는다.
+    다운로드된 항목에서 필요한 보고서 종류를 선태하고 생성 요청하여 사용할 수 있다. 
 
-    * 광고 정보 일괄 다운로드(마스터 기능 이용하기)의 최대 제공 기간은 '최근 2년(730일)'이다.
-    * 데이터가 존재하지 않는 경우 다운로드 링크는 제공되지 않는다.
-    * 일부 항목은 변경 분 데이터를 제공하지 않는다.
-    * 릴리즈 노트 참고 - (http://naver.github.io/searchad-apidoc/#/release-notes)
+    * 대용량 보고서의 최대 제공 기간은 '최근 1년'이다. 유형에 따라 기간이 상이하다.
+    * 조회 기간을 선택하지 않으면 자동으로 전일 데이터로 생성되며 전일 부터 최대 1년까지 선택가능하다.
 
-    https://naver.github.io/searchad-apidoc/#/tags/MasterReport
+    https://naver.github.io/searchad-apidoc/#/tags/StatReport
 
-    사용 예시) 특정 시점을 기준으로 광고 계정에 광고 정보에 대한 마스터리포트를 생성하고, 등록된 모든 광고 정보를 다운로드 할 수 있다.
+    사용 예시) 특정 시점을 기준으로 광고 계정에 광고 정보에 대한 Stat 리포트를 생성하고, 등록된 모든 광고 정보를 다운로드 할 수 있다.
 
     .. code-block:: python
 
@@ -58,7 +56,7 @@ class StatReport(BaseSearchAD):
             raise ValueError(f"incorrect parameters: {err}")
 
     def list(self):
-        """"""
+        """모든 등록된 보고서 작업을 검색한다."""
         return self.call('GET', self.default_path)
 
     def create(self, params):
@@ -68,30 +66,30 @@ class StatReport(BaseSearchAD):
         :type params: dict
 
         **params:**
-          - *item* (`str`) : 제공되는 광고 정보 목록 (네이버에서 제공하는 항목 중에 선택)
-          - *from_time* (`str, optional`) - 특정 시점 (ISO 8601 UTC, 2021-12-01T00:00:00Z)
+          - *reportTp* (`str`) : 제공되는 광고 정보 목록 (네이버에서 제공하는 항목 중에 선택)
+          - *statDt* (`str`) - 특정일 (ISO 8601: 2021-12-01T00:00:00Z, KST: YYYYMMDD)
 
         """
 
         return self.call('POST', self.default_path, params=self._get_params(params))
 
     def get(self, report_job_id):
-        """특정 job_id를 사용해서 해당 마스터 리포트의 작업 상세 정보를 가져온다.
+        """특정 Report Job(보고서 작업)을 검색한다.
 
-        :param job_id: 유효한 마스터 리포트 Job id
-        :type job_id: str
+        :param report_job_id: 유효한 Report Job ID
+        :type report_job_id: str
         """
 
         return self.call('GET', f'{self.default_path}/{report_job_id}')
 
     def delete_all(self):
-        """모든 마스터 리포트 작업을 삭제한다."""
+        """모든 Report Job들을 삭제한다."""
         return self.call('DELETE', self.default_path)
 
     def delete(self, report_job_id):
-        """해당 마스터 리포트를 job id로 삭제한다.
+        """해당 Report Job을 삭제한다.
 
-        :param job_id: 유효한 마스터 리포트 Job id
-        :type job_id: str
+        :param report_job_id: 유효한 Report Job ID
+        :type report_job_id: str
         """
         return self.call('DELETE', f'{self.default_path}/{report_job_id}')
